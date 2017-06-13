4c9e91a279e0cd68006f8907265d416a484569a7
Gloria Budiman <wahyu.g@gmail.com>
2017-06-13 14:45:41 -0700

    Add half-page expander

--- app/assets/javascripts/layout/layout.js
+++ app/assets/javascripts/layout/layout.js
@@ -1,10 +1,30 @@
 var layout = (function() {
+  state = 'half';
+
   var attach = function() {
     $(window).resize(function() {
       set();
     })
 
     set();
+    $('#minmax').on('click', function() {
+      minmax();
+      return false;
+    })
+  }
+
+  var minmax = function() {
+    if (state == 'half') {
+      state = 'right-full';
+      $('#main-left').hide();
+      $('#main-right').removeClass('col-xs-6').addClass('col-xs-12');
+      $('#minmax').text('> Half <');
+    } else if (state == 'right-full') {
+      state = 'half';
+      $('#main-left').show();
+      $('#main-right').removeClass('col-xs-12').addClass('col-xs-6');
+      $('#minmax').text('<< Max');
+    }
   }
 
   var set = function() {
