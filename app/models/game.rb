870f13f0abde32240f7a9737b25e6b4f9e1da803
Gloria Budiman <wahyu.g@gmail.com>
2018-04-20 19:21:16 -0700

    More chaptes

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,7 +6,15 @@ class Game < ApplicationRecord
     #oregon: :parse_oregon,
     #new_jersey: :parse_new_jersey,
     #new_mexico: :parse_new_mexico,
-    ohio: :parse_ohio,
+    #ohio: :parse_ohio,
+    #georgia: :parse_georgia,
+    #mass: :parse_mass,
+    #virginia: :parse_virginia,
+    #new_york: :parse_new_york,
+    #penn: :parse_penn,
+    #oklahoma: :parse_oklahoma,
+    #texas: :parse_texas,
+    indiana: :parse_indiana,
   }
   @@mech = Mechanize.new
   @@year = Time.now.year
@@ -34,6 +42,83 @@ private
       page.search('li.eventlist-meta-item').each do |li|
         li.search('.event-date:first').each do |x|
           Game.find_or_initialize_by(chapter: 'washington',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_georgia
+    @@mech.get('https://dystopiarisinggeorgia.com/calendar-of-events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'georgia',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_mass
+    @@mech.get('http://www.dystopiarisingma.com/calendar-of-events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'mass',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_virginia
+    @@mech.get('http://www.dystopiarisingva.com/events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'virginia',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_new_york
+    @@mech.get('https://www.dystopiarisingny.com/new-events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'new_york',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_penn
+    @@mech.get('https://www.dystopiarisingpenn.com/new-events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'penn',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_oklahoma
+    @@mech.get('http://www.dystopiarisingok.com/events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'oklahoma',
+                                     start: Date.parse(x.text)).save!
+        end
+      end
+    end
+  end
+
+  def self.parse_texas
+    @@mech.get('http://www.idratherbeinbravo.com/events/') do |page|
+      page.search('li.eventlist-meta-item').each do |li|
+        li.search('.event-date:first').each do |x|
+          Game.find_or_initialize_by(chapter: 'texas',
                                      start: Date.parse(x.text)).save!
         end
       end
@@ -116,6 +201,26 @@ private
         elsif month_match and year_state != nil
           date = Date.parse("#{month_match[1]} #{month_match[2]}, #{year_state}")
           Game.find_or_initialize_by(chapter: 'new_jersey',
+                                     start: date).save!
+        end
+
+      end
+    end
+  end
+
+  def self.parse_indiana
+    @@mech.get('http://www.dystopiarisingindiana.com/calendar/') do |page|
+      year_state = nil
+
+      page.search('p').each do |h|
+        year_match = h.text.match(/^(\d+)$/i)
+        month_match = h.text.match(/(\w+)\s+(\d+)\-/)
+
+        if year_match
+          year_state = year_match[1].to_i
+        elsif month_match and year_state != nil
+          date = Date.parse("#{month_match[1]} #{month_match[2]}, #{year_state}")
+          Game.find_or_initialize_by(chapter: 'indiana',
                                      start: date).save!
         end
 
